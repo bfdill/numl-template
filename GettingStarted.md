cee9e8881164844f6f7ca445d71f06d0464d4e87
Ben Dill <ben@dillfamily.org>
2016-05-04 21:37:49 -0400

    BFD | Initial commit of numl getting started example

---
+++ GettingStarted.md
@@ -0,0 +1,14 @@
+# Getting Started
+
+## .NET Core
+Ensure [.NET Core](https://dotnet.github.io/getting-started/) is installed
+```bash
+brew update
+brew install openssl
+brew link --force openssl
+```
+
+Install [official PKG](https://dotnetcli.blob.core.windows.net/dotnet/beta/Installers/1.0.0.001598/dotnet-osx-x64.1.0.0.001598.pkg)
+
+## numl
+[Website](http://numl.net/getting-started/)
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using numl.Model;
+using numl.Supervised.DecisionTree;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var description = Descriptor.Create<Iris>();
+            var data = Iris.Load();
+            var generator = new DecisionTreeGenerator();
+            var model = generator.Generate(description, data);
+            Console.WriteLine("Generated model: ");
+            Console.WriteLine(model);
+        }
+    }
+}
